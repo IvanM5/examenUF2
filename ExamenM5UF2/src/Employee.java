75bce0ef6eb4444b20bfb500d0d9e43eb28e945f
Ivan <belzebuth-88@hotmail.com>
2014-11-18 18:06:08 +0100

    Versio 2

--- ExamenM5UF2/src/Employee.java
+++ ExamenM5UF2/src/Employee.java
@@ -1,33 +1,67 @@
 public class Employee {
+	/**
+	 * @author Iván
+	 * @param myName
+	 *            variable de clase de nom
+	 * @param mySalary
+	 *            variable privada de salari.
+	 */
+	private String myName;
+	private double mySalary;
 
-   private String myName;
-   private double mySalary;
+	/**
+	 * @method funció que relaciona salari amb el propietari
+	 * 
+	 * @param name
+	 *            variable de la funció employee nom
+	 * @param salary
+	 *            variable de la funció employee salari
+	 */
+	public Employee(String name, double salary) {
+		myName = name;
+		mySalary = salary;
+		System.out.println("Soc la segona versio");
+	}
 
-   public Employee(String name, double salary) {
-      myName = name;
-      mySalary = salary;
-      System.out.println("Soc la segona versio");
-   }
+	/**
+	 * 
+	 * @return String
+	 */
 
-   public String getName() {
-      return myName;
-   }
+	public String getName() {
+		return myName;
+	}
 
-   public double getSalary() {
-      return mySalary;
-   }
+	/**
+	 * 
+	 * @return double
+	 */
+	public double getSalary() {
+		return mySalary;
+	}
 
-   public void raiseSalary(int percent) {
-      mySalary = mySalary +
-         percent * 0.01 * mySalary;
-   }
+	/**
+	 * 
+	 * @param percent
+	 * @method Funcio que retorna el salari amb percentatge.
+	 */
 
-   public void setSalary(double salary) {
-      mySalary = salary;
-   }
+	public void raiseSalary(int percent) {
+		mySalary = mySalary + percent * 0.01 * mySalary;
+	}
+	/**
+	 * @method Funcio que estableix un salari determinat.
+	 * @param salary
+	 */
 
-   public String toString() {
-      return "Name: " + getName() +
-             ", Salary: " + getSalary();
-   }
+	public void setSalary(double salary) {
+		mySalary = salary;
+	}
+	/**
+	 * @method Funcio que imprimeix per pantalla el nom i el seu salari.
+	 */
+
+	public String toString() {
+		return "Name: " + getName() + ", Salary: " + getSalary();
+	}
 }
